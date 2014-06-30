1e6edc524c95e6be2a32d34a17c02a49a786cb5a
Bollocks <jqrainwater@yahoo.com>
2014-06-30 20:34:36 +0000

    Add update projects

---
+++ spec/features/projects/editing_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+feature 'Editing projects' do
+  before do
+    project = create :project
+    
+    visit '/'
+    click_link project.name
+    click_link 'Edit Project'
+  end
+  
+  scenario "Updating a project" do
+    fill_in 'Name', with: "TextMate 2 Beta"
+    click_button 'Update Project'
+    
+    expect(page).to have_content 'Project has been updated'
+  end
+  
+  scenario "Updating with bad attributes" do
+    fill_in "Name", with: ""
+    click_button 'Update Project'
+    
+    expect(page).to have_content 'Project has not been updated'
+  end
+end
